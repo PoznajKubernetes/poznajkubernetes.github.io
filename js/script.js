8f6aa5c1730b4bbb53155c3e76116c3b082073a3
Piotr Stapp <piotr.stapp@finai.com>
2019-09-14 20:30:11 +0200

    Timer in lead

--- js/script.js
+++ js/script.js
@@ -362,33 +362,34 @@
     var yourDateToGo = new Date("2019-09-18T21:00:00+02:00"); //new Date(); //here you're making new Date object
     //yourDateToGo.setDate(yourDateToGo.getDate() + 1); //your're setting date in this object 1 day more from now
     //you can change number of days to go by putting any number in place of 1
-    var timing = setInterval(
-      // you're making an interval - a thing, that is updating content after number of miliseconds, that you're writing after comma as second parameter
-      function() {
-        var currentDate = new Date().getTime(); //same thing as above
-        var timeLeft = yourDateToGo - currentDate; //difference between time you set and now in miliseconds
-        var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24)); //conversion miliseconds on days
-        if (days < 10) days = "0" + days; //if number of days is below 10, programm is writing "0" before 9, that's why you see "09" instead of "9"
-        var hours = Math.floor(
-          (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        ); //conversion miliseconds on hours
-        if (hours < 10) hours = "0" + hours;
-        var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)); //conversion miliseconds on minutes
-        if (minutes < 10) minutes = "0" + minutes;
-        var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000); //conversion miliseconds on seconds
-        if (seconds < 10) seconds = "0" + seconds;
+    var timer = function() {
+      var currentDate = new Date().getTime(); //same thing as above
+      var timeLeft = yourDateToGo - currentDate; //difference between time you set and now in miliseconds
+      var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24)); //conversion miliseconds on days
+      if (days < 10) days = "0" + days; //if number of days is below 10, programm is writing "0" before 9, that's why you see "09" instead of "9"
+      var hours = Math.floor(
+        (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      ); //conversion miliseconds on hours
+      if (hours < 10) hours = "0" + hours;
+      var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)); //conversion miliseconds on minutes
+      if (minutes < 10) minutes = "0" + minutes;
+      var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000); //conversion miliseconds on seconds
+      if (seconds < 10) seconds = "0" + seconds;
+      //which id is countdown
+      if (timeLeft <= 0) {
+        clearInterval(timing);
+      } else {
         //console.log(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
         document.getElementById("countdown").innerHTML =
-          "za " + days + "d " + hours + "h " + minutes + "m " + seconds + "s"; // putting number of days, hours, minutes and seconds in div,
-        //which id is countdown
-        if (timeLeft <= 0) {
-          clearInterval(timing);
-          document.getElementById("countdown").innerHTML = "koniec"; //if there's no time left, programm in this 2 lines is clearing interval (nothing is counting now)
-          //and you see "It's over" instead of time left
-        }
-      },
-      1000
+          "za " + days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+        document.getElementById("countdown2").innerHTML =
+          "jeszcze <strong>" + days + "d " + hours + "h " + minutes + "m " + seconds + "s</strong>";
+      }
+    }
+    var timing = setInterval(
+      timer,1000
     );
+    timer();
   });
   $(document).ready(function() {
     var template = $("#faq-template").html();
