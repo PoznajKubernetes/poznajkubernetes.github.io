dd321e9dc42700f6ec1e1abb8942e725b51b5d84
Piotr Stapp <piotr.stapp@finai.com>
2019-11-02 21:52:46 +0100

    Add "kup" button

--- js/script.js
+++ js/script.js
@@ -359,7 +359,7 @@
       $("#agenda-menu").click();
       return false;
     });
-    var yourDateToGo = new Date("2019-09-18T20:00:00+02:00"); //new Date(); //here you're making new Date object
+    var yourDateToGo = new Date("2019-11-15T21:00:00+01:00"); //new Date(); //here you're making new Date object
     //yourDateToGo.setDate(yourDateToGo.getDate() + 1); //your're setting date in this object 1 day more from now
     //you can change number of days to go by putting any number in place of 1
     var timer = function() {
@@ -376,22 +376,21 @@
       var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000); //conversion miliseconds on seconds
       if (seconds < 10) seconds = "0" + seconds;
       //which id is countdown
+      var end = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
       if (timeLeft <= 0) {
         clearInterval(timing);
         days = "00";
         hours = "00";
         minutes = "00";
         seconds = "00";
+        end = "";
       }
 
       //console.log(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
       try {
-      	document.getElementById("countdown").innerHTML =
-          "";
-      	document.getElementById("countdown2").innerHTML =
-          "";
-      	document.getElementById("countdown3").innerHTML =
-          "";
+      	document.getElementById("countdown").innerHTML = end;
+      	document.getElementById("countdown2").innerHTML = end;
+      	document.getElementById("countdown3").innerHTML = end;
       } catch {
       }
     }
