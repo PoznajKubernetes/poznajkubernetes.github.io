37743f6c248bb639865c4f4e85fdf12c0a690c81
Piotrek Stapp <piotr.stapp@allegro.pl>
2020-04-14 21:02:16 +0200

    Defer images loading v2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+if ('loading' in HTMLImageElement.prototype) {
+  const images = document.querySelectorAll("img.lazyload");
+  images.forEach(img => {
+      img.src = img.dataset.src;
+  });
+} else {
+  // Dynamically import the LazySizes library
+let script = document.createElement("script");
+script.async = true;
+script.src =
+  "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+document.body.appendChild(script);
+}
 (function ($) {
   "use strict";
 
